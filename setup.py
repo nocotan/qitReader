56b03cde70aeaa14d995daf95a93726eccb1955d
nocotan <noconoco.lib@gmail.com>
2016-06-01 03:36:46 +0900

    setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
-''' qitReader
-setup.py
+from setuptools import setup, find_packages
 
-created by @nocotan
-'''
+setup(name='qitreader',
+      version='0.0.1',
+      description='Qiita Reader in konsole',
+      author='@nocotan',
+      author_email='noconoco.lib@gmail.com',
+      url='https://github.com/nocotan/qitReader',
+      packages=find_packages(where='*'),
+      entry_points="""
+      [console_scripts]
+      qitreader = qit_reader.qit_reader:qitReader.qit_reader
+      """,)
